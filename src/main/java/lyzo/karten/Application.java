eef2ea34e8b0caf9930fe06ce4354cf7a34a6cf8
ErrorTeapot <lyzoivan@gmail.com>
2026-01-01 17:41:04 -0500

    PATCH 0.0.2
    Update Application file to prepare stage and display scene (empty, just a label for now)

--- src/main/java/lyzo/karten/Application.java
+++ src/main/java/lyzo/karten/Application.java
@@ -1,4 +1,29 @@
 package lyzo.karten;
 
-public class Application {
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    // program entry point; calls start method below
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // prepare stage (window)
+        stage.setTitle("Karten");
+        stage.setMaximized(true);
+
+        // prepare scene (window content)
+        Scene scene = new Scene(new Label("Hello, World!"));
+
+        // add scene
+        stage.setScene(scene);
+
+        // show stage
+        stage.show();
+    }
 }
